83952315d0345a5648047ae6c7927967cc032935
Subaang <146642625+Subaang@users.noreply.github.com>
2024-02-22 12:44:03 +0530

    Add table calculations

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const table = document.querySelector("#table");
 
 go.addEventListener('click',(event) => {
     const n = document.querySelector("#n").value;
-    console.log(n);
     let calc = document.createElement('button');
     calc.className = "calc";
     calc.textContent = "calculate";
@@ -19,7 +18,7 @@ go.addEventListener('click',(event) => {
 
     
     table.insertAdjacentHTML('beforeend',"<input type = 'text' class='in' value='x_i' disabled>");
-    table.insertAdjacentHTML('beforeend',"<input type = 'text' class='in' value='x_i' disabled>");
+    table.insertAdjacentHTML('beforeend',"<input type = 'text' class='in' value='y_i' disabled>");
     table.insertAdjacentHTML('beforeend',"<input type = 'text' class='in' value='x_i - x_bar' disabled>");
     table.insertAdjacentHTML('beforeend',"<input type = 'text' class='in' value='(x_i - x_bar)y_i' disabled>");
     table.insertAdjacentHTML('beforeend',"<div class='break'></div>");
@@ -47,7 +46,7 @@ go.addEventListener('click',(event) => {
         d = document.createElement('input');
         d.type = 'text';
         d.disabled = 'disabled';
-        d.classList.add('in','numberator_'+i);
+        d.classList.add('in','numerator_'+i);
         table.appendChild(d);
 
         table.insertAdjacentHTML('beforeend',"<div class='break'></div>");
@@ -57,6 +56,30 @@ go.addEventListener('click',(event) => {
 
     
     calc.addEventListener('click', () => {
+        for(let i = 0; i < n; i++){
+            let x_bar = 0, y_bar = 0;
+            // finding x_bar and y_bar
+            for(let i = 0; i < n; i++){
+                x_bar += parseFloat(document.querySelector('.x_i_'+i).value);
+                y_bar += parseFloat(document.querySelector('.y_i_'+i).value);
+            }
+            x_bar /= n;
+            y_bar /= n;
+            //-----
+            
+            let x_i, y_i,third, numerator;
+
+            for(let i = 0; i < n; i++){
+                x_i = parseFloat(document.querySelector('.x_i_'+i).value);
+                y_i = parseFloat(document.querySelector('.y_i_'+i).value);
+
+                console.log(x_i);
+                
+                document.querySelector('.x_i-mean_'+i).value = x_i - x_bar;
+                document.querySelector('.numerator_'+i).value = (x_i - x_bar)*y_i;
+
+            }
+        }
         
     })
 
